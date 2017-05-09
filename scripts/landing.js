ddcc97d0712ceeffe26da68bfed701af2201a562
sunnydgoswami <sunnydgoswami@gmail.com>
2017-05-08 21:29:05 -0400

    bloc-jams code starting at checkpoint 12

---
+++ scripts/landing.js
@@ -0,0 +1,27 @@
+var points = document.getElementsByClassName('point');
+
+var animatePoints = function (points) {
+
+  for (var i = 0; i < points.length; i++) {
+    points[i].style.opacity = 1
+    points[i].style.transform = "scaleX(1) translateY(0)";
+    points[i].style.msTransform = "scaleX(1) translateY(0)";
+    points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
+  }
+}
+
+window.onload = function () {
+
+   if (window.innerHeight > 950) {
+       animatePoints(pointsArray);
+   }
+
+   var sellingPoints = document.getElementsByClassName('selling-points')[0];
+   var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+
+   window.addEventListener('scroll', function(event) {
+     if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+             animatePoints(points);
+         }
+  });
+}
