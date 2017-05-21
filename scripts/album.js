1a697cc8b1a6c8e2194f3f604406c197a45b2c15
sunnydgoswami <sunnydgoswami@gmail.com>
2017-05-21 11:37:57 -0400

    IGNORE

--- scripts/album.js
+++ scripts/album.js
@@ -68,7 +68,7 @@ var createSongRow = function(songNumber, songName, songLength) {
              var $volumeFill = $('.volume .fill');
              var $volumeThumb = $('.volume .thumb');
              $volumeFill.width(currentVolume + '%');
-             $volumeThumb.css({left: currentVolume + '%'});             
+             $volumeThumb.css({left: currentVolume + '%'});
              updatePlayerBarSong();
      	 } else if (currentlyPlayingSongNumber === songNumber) {
              if (currentSoundFile.isPaused()){
@@ -255,3 +255,6 @@ var createSongRow = function(songNumber, songName, songLength) {
         });
  });
 };
+
+
+"SCHIND"
